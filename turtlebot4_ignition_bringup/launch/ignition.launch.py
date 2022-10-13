decdf2b0ef15548df3826acd9ce75d0e34239dce
techlabs <hugo.borne-pons@accenture.com>
2022-10-13 18:25:41 +0200

    fix missing argument declaration in launch file

--- turtlebot4_ignition_bringup/launch/ignition.launch.py
+++ turtlebot4_ignition_bringup/launch/ignition.launch.py
@@ -76,6 +76,10 @@ ARGUMENTS = [
                           description='robot namespace'),
 ]
 
+for pose_element in ['x', 'y', 'z', 'yaw']:
+    ARGUMENTS.append(DeclareLaunchArgument(pose_element, default_value='0.0',
+                     description=f'{pose_element} component of the robot pose.'))
+
 
 def generate_launch_description():
 
